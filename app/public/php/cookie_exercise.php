c40168af36b6ec8c8a1680d1f1d0c6aee86bd6e5
Thanh Vo <tinhthanh.vo@nfq.asia>
2022-02-19 23:51:30 +0700

    refactor code for cookie and drawing rectangle star exercise

--- app/public/php/cookie_exercise.php
+++ app/public/php/cookie_exercise.php
@@ -1,52 +1,17 @@
 <?php
-if (!isset($_COOKIE['animal'])) {
-    if (empty($_POST['animal'])) {
-        setcookie("animal", '', time() + 3600, '/');
-    } else {
-        setcookie("animal", $_POST['animal'], time() + 3600, '/');
+if(!isset($_COOKIE['my_animal'])){
+    if(empty($_POST['my_animal'])){
+        setcookie("my_animal", '', time() + 3600, '/');
     }
-} elseif (!empty($_POST['animal']) && $_COOKIE['animal'] != $_POST['animal']) {
-    setcookie("animal", $_POST['animal'], time() + 3600, '/');
+    else{
+        setcookie("my_animal", $_POST['my_animal'], time() + 3600, '/');
+    }
 }
-?>
-<!Doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <title>List Choice use Cookie</title>
-</head>
-<body>
-    <div>
-        <h2>Please check the one of the animals which you love: </h2>
+else if(!empty($_POST['my_animal']) && $_COOKIE['my_animal']!=$_POST['my_animal']){
+    setcookie("my_animal", $_POST['my_animal'], time() + 3600, '/');
+}
+echo "<h2>The animal which you have at home</h2><br>";
 
-        <form method="post" action="index.php">
-            <input type="radio" name="animal" value="cat" id="animal" <?php if (count($_COOKIE) > 0 && $_COOKIE['animal'] == 'cat') {
-                echo "checked";
-                                                                      }?>>
-            <label for="animal"> I love cat</label><br><br>
-
-            <input type="radio" name="animal" value="dog" id="animal" <?php if (count($_COOKIE) > 0 && $_COOKIE['animal'] == 'dog') {
-                echo "checked";
-                                                                      }?>>
-            <label for="animal"> I love dog</label><br><br>
-
-            <input type="radio" name="animal" value="snake" id="animal" <?php if (count($_COOKIE) > 0 && $_COOKIE['animal'] == 'snake') {
-                echo "checked";
-                                                                        }?>>
-            <label for="animal"> I love snake</label><br><br>
-
-            <input type="radio" name="animal" value="bird" id="animal" <?php if (count($_COOKIE) > 0 && $_COOKIE['animal'] == 'bird') {
-                echo "checked";
-                                                                       }?>>
-            <label for="animal"> I love bird</label><br><br>
-            <input type="submit" value="Submit">
-        </form>
-    </div>
-    <div>
-        <a href="index.php"> Back</a>
-    </div>
-
-</body>
-</html>
+if(isset($_COOKIE['my_animal']))
+    echo "<p>".$_COOKIE['my_animal']."</p>";
+else echo "Set cookie is false.";
--- app/public/php/drawing_rectangle_star.php
+++ app/public/php/drawing_rectangle_star.php
@@ -1,70 +1,37 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Drawing Rectangle Star</title>
-</head>
-<body>
-    <div>
-        <?php
-            $array_number = '';
-        if (isset($_POST["width"]) && isset($_POST["height"])) {
-            $width = $_POST['width'];
-            $height = $_POST['height'];
-        } else {
-            $width = 0;
-            $height = 0;
+<?php
+$array_number = '';
+if (isset($_POST["width"]) && isset($_POST["height"])) {
+    $width = $_POST['width'];
+    $height = $_POST['height'];
+}
+else{
+    $width = 0;
+    $height = 0;
+}
+
+if(isset($_POST['height']) && isset($_POST['width'])){
+    if($width == 1 && $height == 1) echo "*";
+    else{
+        $width = $_POST['width'];
+        $height = $_POST['height'];
+
+        for($j=0; $j<$width; $j++){
+            echo "* ";
         }
-        ?>
-    </div>
+        echo "<br>";
 
-    <div>
-        <h2>Please input width and height for the Rectangle Star</h2>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <label for="width">Input width: <span style="color: red"> *</span></label>
-            <input type="number" id="width" name="width" value="<?php if (isset($width)) {
-                echo $width;
-                                                                }?>" required placeholder="Please typing integer, Ex: 1">
-            <br><br>
-            <label for="height">Input height: <span style="color: red"> *</span></label>
-            <input type="number" id="height" name="height" value="<?php if (isset($height)) {
-                echo $height;
-                                                                  }?>" required placeholder="Please typing integer, Ex: 1">
-            <br><br>
-            <input type="submit" name="submit" value="Draw" style="background-color: cyan">
-        </form>
-    </div>
+        for($i=1; $i<$height-1; $i++){
+            echo "* ";
+            for($j=1; $j<$width-1; $j++){
+                echo "&nbsp&nbsp&nbsp";
+            }
+            echo "*<br>";
+        }
 
-    <div>
-        <?php
-        if (isset($_POST['height']) && isset($_POST['width'])) {
-            if ($width == 1 && $height == 1) {
-                echo "*";
-            } else {
-                $width = $_POST['width'];
-                $height = $_POST['height'];
-
-                for ($j = 0; $j < $width; $j++) {
-                    echo "* ";
-                }
-                echo "<br>";
-
-                for ($i = 1; $i < $height - 1; $i++) {
-                    echo "* ";
-                    for ($j = 1; $j < $width - 1; $j++) {
-                        echo "&nbsp&nbsp&nbsp";
-                    }
-                    echo "*<br>";
-                }
-
-                if ($height > 1) {
-                    for ($j = 0; $j < $width; $j++) {
-                        echo "* ";
-                    }
-                }
+        if($height >1){
+            for($j=0; $j<$width; $j++){
+                echo "* ";
             }
         }
-        ?>
-    </div>
-</body>
-</html>
+    }
+}
