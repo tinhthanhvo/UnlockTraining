e35cb18bc7a586dfd9d7a24b18db491e6dcf7825
Thanh Vo <tinhthanh.vo@nfq.asia>
2022-02-24 16:11:01 +0700

    modifiled exercise, finding number, cookie file and add create redirect page

--- app/public/php/FindingNumber.php
+++ app/public/php/FindingNumber.php
@@ -1,9 +1,3 @@
-<?php
-if(isset($_POST['animal'])){
-    $_COOKIE['animal'] = $_POST['animal'];
-    setcookie("animal", $_POST['animal'], time() + 5, '/');
-}
-?>
 <!DOCTYPE HTML>
 <html lang="en">
 <head>
@@ -12,15 +6,20 @@ if(isset($_POST['animal'])){
     <title>Exercise PHP</title>
 </head>
 <body>
-
+<?php
+if (isset($_POST['animal'])) {
+    $_COOKIE['animal'] = $_POST['animal'];
+    setcookie("animal", $_POST['animal'], time() + 5, '/');
+}
+?>
     <div>
         <?php
             $array_number = '';
-            if (empty($_POST["array_number"])) {
-                $array_numberErr = "Name is required";
-            } else {
-                $array_number = $_POST["array_number"];
-            }
+        if (empty($_POST["array_number"])) {
+            $array_numberErr = "Name is required";
+        } else {
+            $array_number = $_POST["array_number"];
+        }
         ?>
     </div>
     <div>
@@ -28,7 +27,9 @@ if(isset($_POST['animal'])){
         <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
             <label for="array_number">Input Array Number</label>
             <input type="text" id="array_number" name="array_number"
-                   value="<?php if (isset($array_number)) echo $array_number;?>" required
+                   value="<?php if (isset($array_number)) {
+                        echo $array_number;
+                          }?>" required
                    placeholder="Ex: 1,2,3...">
             <br>
             <label id="alert" style="color: red"></label>
@@ -38,63 +39,67 @@ if(isset($_POST['animal'])){
     </div>
     <div>
         <?php
-            $formated_input = str_replace(","," ", $array_number);
-            echo("The list input number is formated: ".$formated_input);
+            $formated_input = str_replace(",", " ", $array_number);
+            echo("The list input number is formated: " . $formated_input);
             $array_numbers = explode(" ", $formated_input);
-            if(isset($array_numbers)){
-                echo("The list input number is formated: ");
-                foreach ($array_numbers as $number){
-                    echo($number." ");
-                }
-                echo('<br>');
+        if (isset($array_numbers)) {
+            echo("The list input number is formated: ");
+            foreach ($array_numbers as $number) {
+                echo($number . " ");
             }
+            echo('<br>');
+        }
 
-            function find_negative_number($array_numbers){
-                foreach ($array_numbers as $number){
-                    if($number < 0){
-                        $array_negative_numbers[] = $number;
-                    }
+        function find_negative_number($array_numbers)
+        {
+            foreach ($array_numbers as $number) {
+                if ($number < 0) {
+                    $array_negative_numbers[] = $number;
                 }
-                if(isset($array_negative_numbers))
-                    return $array_negative_numbers;
             }
+            if (isset($array_negative_numbers)) {
+                return $array_negative_numbers;
+            }
+        }
 
-            function find_positive_number($array_numbers){
-                foreach ($array_numbers as $number){
-                    if(!($number < 0)){
-                        $array_positive_numbers[] = $number;
-                    }
+        function find_positive_number($array_numbers)
+        {
+            foreach ($array_numbers as $number) {
+                if (!($number < 0)) {
+                    $array_positive_numbers[] = $number;
                 }
-                if(isset($array_positive_numbers))
-                    return $array_positive_numbers;
             }
+            if (isset($array_positive_numbers)) {
+                return $array_positive_numbers;
+            }
+        }
 
             $array_negative_numbers = find_negative_number($array_numbers);
             $array_positive_numbers = find_positive_number($array_numbers);
-            echo("The greatest number is: ".max($array_positive_numbers).'<br>');
-            echo("The smallest number is: ".min($array_numbers).'<br>');
-            if(isset($array_negative_numbers)){
-                echo("The list negative number is: ");
-                foreach ($array_negative_numbers as $number){
-                    echo($number." ");
-                }
-                echo('<br>');
+            echo("The greatest number is: " . max($array_positive_numbers) . '<br>');
+            echo("The smallest number is: " . min($array_numbers) . '<br>');
+        if (isset($array_negative_numbers)) {
+            echo("The list negative number is: ");
+            foreach ($array_negative_numbers as $number) {
+                echo($number . " ");
             }
+            echo('<br>');
+        }
 
-            if(isset($array_positive_numbers)){
-                echo("The list positive number is: ");
-                foreach ($array_positive_numbers as $number){
-                    echo($number." ");
-                }
+        if (isset($array_positive_numbers)) {
+            echo("The list positive number is: ");
+            foreach ($array_positive_numbers as $number) {
+                echo($number . " ");
             }
+        }
         ?>
     </div>
 
     <div>
         <?php
-            if(isset($_COOKIE['animal'])){
-                echo "<h2>* The animal which you love is  ".$_COOKIE['animal']."</h2>";
-            }
+        if (isset($_COOKIE['animal'])) {
+            echo "<h2>* The animal which you love is  " . $_COOKIE['animal'] . "</h2>";
+        }
         ?>
     </div>
 
