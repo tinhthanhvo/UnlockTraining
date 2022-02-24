77e364853f81b62318faa46745a33985c4a56f69
Thanh Vo <tinhthanh.vo@nfq.asia>
2022-02-24 23:04:23 +0700

    refactor code and edit view for finding number exercise

--- app/public/php/FindingNumber.php
+++ app/public/php/FindingNumber.php
@@ -1,133 +1,43 @@
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-    <style>
-    </style>
-    <title>Exercise PHP</title>
-</head>
-<body>
 <?php
-if (isset($_POST['animal'])) {
-    $_COOKIE['animal'] = $_POST['animal'];
-    setcookie("animal", $_POST['animal'], time() + 5, '/');
+
+$formInput = $_POST['numberArray'];
+$inputFormat = str_replace(",", " ", $formInput);
+$inputFormat = explode(" ", $inputFormat);
+
+include('../utils/FindingNumberFunction.php');
+
+$numberStringArray = array_filter($inputFormat, "is_numeric");
+$numberArray = array_map("convertStrToInt", $numberStringArray);
+$response = "<ul class='list-group list-group-flush'>";
+if (count($numberArray) != 0) {
+    $negativeNumberArray = array_filter($numberArray, "isNegativeNumber");
+    $positiveNumberArray = array_filter($numberArray, "isPositiveNumber");
+    if(count($positiveNumberArray) > 0){
+        $maximumNumber = max($positiveNumberArray);
+    }
+    elseif (count($negativeNumberArray) > 0){
+        $maximumNumber = max($negativeNumberArray);
+    }
+    else{
+        $maximumNumber = "NONE";
+    }
+
+    if(count($negativeNumberArray) > 0){
+        $minimumNumber = min($negativeNumberArray);
+    }
+    elseif (count($positiveNumberArray) > 0){
+        $minimumNumber = min($positiveNumberArray);
+    }
+    else{
+        $minimumNumber = "NONE";
+    }
+
+    $response = $response ."<li class='list-group-item'>The number input list: ".printArray($numberArray)."</li>";
+    $response = $response ."<li class='list-group-item'>The negative number list: ".printArray($negativeNumberArray)."</li>";
+    $response = $response ."<li class='list-group-item'>The positive number list: ".printArray($positiveNumberArray)."</li>";
+    $response = $response ."<li class='list-group-item'>The maximum number is : ".$maximumNumber."</li>";
+    $response = $response ."<li class='list-group-item'>The minimum number is : ".$minimumNumber."</li></ul>";
+} else {
+    $response = $response ."The array input is null or not number array";
 }
-?>
-    <div>
-        <?php
-            $array_number = '';
-        if (empty($_POST["array_number"])) {
-            $array_numberErr = "Name is required";
-        } else {
-            $array_number = $_POST["array_number"];
-        }
-        ?>
-    </div>
-    <div>
-        <h2>Please input array number (different by ",")</h2>
-        <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-            <label for="array_number">Input Array Number</label>
-            <input type="text" id="array_number" name="array_number"
-                   value="<?php if (isset($array_number)) {
-                        echo $array_number;
-                          }?>" required
-                   placeholder="Ex: 1,2,3...">
-            <br>
-            <label id="alert" style="color: red"></label>
-            <br>
-            <input type="submit" name="submit" value="Submit" onclick="return checkInputValidation()">
-        </form>
-    </div>
-    <div>
-        <?php
-            $formated_input = str_replace(",", " ", $array_number);
-            echo("The list input number is formated: " . $formated_input);
-            $array_numbers = explode(" ", $formated_input);
-        if (isset($array_numbers)) {
-            echo("The list input number is formated: ");
-            foreach ($array_numbers as $number) {
-                echo($number . " ");
-            }
-            echo('<br>');
-        }
-
-        function find_negative_number($array_numbers)
-        {
-            foreach ($array_numbers as $number) {
-                if ($number < 0) {
-                    $array_negative_numbers[] = $number;
-                }
-            }
-            if (isset($array_negative_numbers)) {
-                return $array_negative_numbers;
-            }
-        }
-
-        function find_positive_number($array_numbers)
-        {
-            foreach ($array_numbers as $number) {
-                if (!($number < 0)) {
-                    $array_positive_numbers[] = $number;
-                }
-            }
-            if (isset($array_positive_numbers)) {
-                return $array_positive_numbers;
-            }
-        }
-
-            $array_negative_numbers = find_negative_number($array_numbers);
-            $array_positive_numbers = find_positive_number($array_numbers);
-            echo("The greatest number is: " . max($array_positive_numbers) . '<br>');
-            echo("The smallest number is: " . min($array_numbers) . '<br>');
-        if (isset($array_negative_numbers)) {
-            echo("The list negative number is: ");
-            foreach ($array_negative_numbers as $number) {
-                echo($number . " ");
-            }
-            echo('<br>');
-        }
-
-        if (isset($array_positive_numbers)) {
-            echo("The list positive number is: ");
-            foreach ($array_positive_numbers as $number) {
-                echo($number . " ");
-            }
-        }
-        ?>
-    </div>
-
-    <div>
-        <?php
-        if (isset($_COOKIE['animal'])) {
-            echo "<h2>* The animal which you love is  " . $_COOKIE['animal'] . "</h2>";
-        }
-        ?>
-    </div>
-
-    <div style="margin-top: 10px">
-        <h2>
-            <a href="cookie_exercise.php"><?php isset($_COOKIE['animal']) ? print "Change option" : print "Chose the animal, you love."?></a>
-        </h2>
-        <h2>
-            <a href="drawing_rectangle_star.php">Exercise 3</a>
-        </h2>
-    </div>
-
-    <script>
-        function checkInputValidation() {
-            let input_string = document.getElementById("array_number").value;
-            let number_string = input_string.replace(/[, ]+/g,' ');
-            let number_array = number_string.split(' ');
-            let flag = true;
-            for (let number of number_array) {
-                if(isNaN(number)) {
-                    flag = false;
-                    let element = document.getElementById("alert");
-                    element.innerHTML = 'Please enter input is string number!';
-                }
-            }
-            return flag;
-        }
-        </script>
-</body>
-</html>
-
+echo $response;
---
+++ app/public/utils/FindingNumberFunction.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * @param string $numberString
+ * @return int
+ */
+function convertStrToInt(string $numberString): int
+{
+    return (int) $numberString;
+}
+
+/**
+ * @param int $number
+ * @return bool
+ */
+function isNegativeNumber(int $number = 0): bool
+{
+    return $number < 0;
+}
+
+/**
+ * @param int $number
+ * @return bool
+ */
+function isPositiveNumber(int $number = 0): bool
+{
+    return $number > 0;
+}
+
+/**
+ * @param array<int> $array
+ * @return string
+ */
+function printArray(array $array = []): string
+{
+    $resultString = '';
+    if (count($array) == 0) {
+        $resultString = $resultString ."None";
+    } else {
+        foreach ($array as $item) {
+            $resultString = $resultString .$item . "&nbsp&nbsp";
+        }
+    }
+    return $resultString;
+}
