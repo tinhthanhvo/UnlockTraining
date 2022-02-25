a5863a183611048ae43714eb982dbaceaa5c5d59
Thanh Vo <tinhthanh.vo@nfq.asia>
2022-02-25 17:04:44 +0700

    Connecting to the database

--- app/public/database/connectDB.php
+++ app/public/database/connectDB.php
@@ -1,1 +1,17 @@
 <?php
+/**
+ * @return mysqli|void
+ */
+function connectDB(){
+    $servername = "db";
+    $username="root";
+    $dbname="training_nfq";
+    $password="password";
+
+    $connection = mysqli_connect($servername,$username,$password,$dbname,3306);
+
+    if(!$connection){
+        die('Could not connect');
+    }
+    return $connection;
+}
